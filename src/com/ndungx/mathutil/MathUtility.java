9de17acd54b8da0e90857bf37cc81c71dbc4f17f
ndungx <dpron12345@gmail.com>
2021-03-22 08:53:51 +0700

    add test case

---
+++ src/com/ndungx/mathutil/MathUtility.java
@@ -0,0 +1,38 @@
+package com.ndungx.mathutil;
+
+/*
+ * @author NDungx
+ */
+public class MathUtility {
+
+    //Class này dùng để chứa các methods dùng trong toán học.
+    //java.util.Math của JDK
+    // vì là đồ xài chung cho nên là static
+    public static final double PI = 3.1415;
+
+    //hàm tính n!
+    //Vì giai thừa tăng rất nhanh, 21! tràn kiểu long
+    //kiểu long chịu đc 10^18 18 con số 0, lớn hơn nữa là thua
+    //âm giai thừa ko tính đc
+    //hàm chỉ chấp nhận từ 0..20! -> long
+    //0! = 1! = 1
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("Invalid argument n must be"
+                    + " between 0..20");
+        }
+
+        if (n == 0 || n == 1) { //khỏi mắc công tính, return ngay
+            return 1;
+        }
+
+        //đến đc đây thì sure kèo n = 0..20
+        long result = 1;//cố tình để value này
+
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        //n! = 1.2.3.4...n
+        return result;
+    }
+}
---
+++ test/com/ndungx/mathutil/test/MathUtilityTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ndungx.mathutil.test;
+
+import static com.ndungx.mathutil.MathUtility.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author NDungx
+ */
+public class MathUtilityTest {
+    
+    @Test
+    public void testFactorialGivenRightArgumentReturnsGoodResult() {
+        int n = 5;  // tui muốn test 5! có đúng = 120 hay ko
+        long expected = 120;
+        long actual = com.ndungx.mathutil.MathUtility.getFactorial(n);
+        assertEquals(expected, actual);
+
+        //mình test tiếp các trường hợp khác đúng đầu vào, đúng đầu ra
+        //khỏi cần biến trung gian, chơi luôn trong lệnh
+        assertEquals(720, getFactorial(6));
+        assertEquals(24, getFactorial(4));
+        assertEquals(6, getFactorial(3));
+        assertEquals(1, getFactorial(1));
+        assertEquals(1, getFactorial(0));
+    }
+
+    //điều gì xảy ra nếu đưa vào tham số sai (âm hoặc lớn hơn 20
+    //theo thiết kế của hàm; đưa vào sai sẽ ném ra ngoại lệ
+    //tức là cần test xem, NGOẠI LỆ NẾU CÓ ĐƯỢC NÉM RA KHÔNG  NẾU TA TEST SAI
+    //THẤY NGOẠI LỆ KHI TA SAI NHƯNG MỪNG VÌ NÓ ĐÚNG
+    @Test(expected = IllegalArgumentException.class)
+    //có đúng là hàm này thì ném ra ngoại lệ tên là illegal argument
+    //nếu đúng thì ném ra ngoại lệ cùng lên, code đúng rồi -> ra màu xanh
+    public void testFactorialGivenWrongArgumentThrowException() {
+        //mình test tình huống hàm phải ném ra ngoại lệ nếu tham số sai
+        //ngoại lệ là không phải value để so sánh, không dùng assertEquals()
+        //ta dùng chiêu khác. JUnit 4 Khác JUnit 5 ở chỗ bắt ngoại lệ
+        getFactorial(-5);
+    }
+
+    //thêm chữ 'test,' vào sau 'init,compile,' ở dòng 1204 trong build-impl.xml
+    //để khi clean and build nếu bị lỗi logic (uncheck) sẽ không tạo file jar
+    // coppy 2 file jar ở \Program Files\NetBeans 8.2 RC\platform\modules\ext
+    // để server github đem về tự compiled giúp
+    
+}
